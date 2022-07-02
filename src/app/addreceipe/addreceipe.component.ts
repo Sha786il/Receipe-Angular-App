31a317e21980f931273447587c5b7fed3c22fc14
Sha786il <shaheensayed2001@gmail.com>
2022-07-02 15:56:37 +0530

    receipe

---
+++ src/app/addreceipe/addreceipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addreceipe',
+  templateUrl: './addreceipe.component.html',
+  styleUrls: ['./addreceipe.component.css']
+})
+export class AddreceipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddreceipeComponent } from './addreceipe/addreceipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddreceipeComponent
   ],
   imports: [
     BrowserModule,
